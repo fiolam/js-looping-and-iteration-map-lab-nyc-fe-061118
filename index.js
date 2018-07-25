2720dbc20836b2e692d81d38d393c2c6b7c036cd
fiolam <fiola.mulliqi@wework.com>
2018-07-25 22:20:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file.
 let driver = ["AAA", "BBB" , "CCC" , "DDD"]
 
-function lowerCaseDrivers(drivers)=nameToAttributes
-let drivers=
+function lowerCaseDrivers(){
+
+}
+  driver.map()
