6deb4f8094decb7bcdaa068611106a7f1b71ec6b
fiolam <fiola.mulliqi@wework.com>
2018-07-25 22:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file.
 let driver = ["AAA", "BBB" , "CCC" , "DDD"]
 
-function lowerCaseDrivers(drivers){
-  let copyDrivers = {...drivers}
-  return drivers ;
+//function lowerCaseDrivers(drivers){
+//  let copyDrivers = {...drivers}
+ // return drivers ;
 }
  // let nameToAttributes = function (drivers){
   //return name + lastName
