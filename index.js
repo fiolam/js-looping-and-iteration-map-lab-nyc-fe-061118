1dbce03e1f66aa8a00b9a4c9d83954a58d90356e
fiolam <fiola.mulliqi@wework.com>
2018-07-25 22:30:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ let driver = ["AAA", "BBB" , "CCC" , "DDD"]
 
 function lowerCaseDrivers(drivers){
   let copyDrivers = {...drivers} 
+  let 
 return firstName + lastName
 
  console.drivers=
 return x;
 }
   driver.map(lowerCaseDrivers())
+  
+  
+  Let names = [“donna,”carl”,”fiola”,”jeff”]
+let excitedNames = function (name){
+return name+”!!”
+////// NEW WAY ////  let excitedNameds=name=>name +”!!”
+names.map(excitedNames)
+
+week 6 day 2
