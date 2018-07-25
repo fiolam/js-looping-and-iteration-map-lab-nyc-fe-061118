92f7722970fb331c2a8af509f6c7547a7d141ae5
fiolam <fiola.mulliqi@wework.com>
2018-07-25 22:24:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let driver = ["AAA", "BBB" , "CCC" , "DDD"]
 
 function lowerCaseDrivers(drivers){
-  function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){- 
+  let copyDrivers={...drivers}
 x.toLowerCase();
 return x;
 }
